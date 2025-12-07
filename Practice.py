dae84a5dcbfab2bbfa3a7edaba9d3d5888eedd62
muhammad-dev78 <muhammad.hussein.hassan78@gmail.com>
2025-12-07 11:19:28 +0300

    My first python practice file

---
+++ Practice.py
@@ -0,0 +1,13 @@
+# My first python practice file
+# Date December 7, 2025
+
+name = "muhammad"
+age = 21
+location = "msa"
+goal = "Full Stack Developer"
+
+print("Name:", name)
+print("Age:", age)
+print("Location:", location)
+print("Goal:", goal)
+print("Inshallah I will achieve my goal!")
